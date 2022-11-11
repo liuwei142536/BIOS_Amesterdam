5f1591199ce43a2b7e68d6957f631a0796bb2010
Shicheng Zhang(LUXSHARE-ICT) <21000809@luxshare.com.cn>
2022-11-11 17:09:47 +0800

    【问题】Mac地址概率性错误
    问题原因：从硬件原理图看出GPIO18输出到RESET_FPGA_FLASH上，当前GPO_BLINK寄存器配置的默认值是0x40000,即GPO_BLINK寄存器
    
    的BIT18配置的是1，所以GPIO18具有BLINK属性,GPIO18信号会每隔0.5s周期性变化，当跳到高电平时，会触发RESET信号，
    
    导致FPGA配置mac地址失败，mac地址从而错误。
    
    修改方式：重新配置GPO_BLINK寄存器，关闭GPIO18的blink属性,使GPIO18的信号保持稳定的低电平，不会出现信号闪烁。
    
    Signed-off-by: Shicheng Zhang(LUXSHARE-ICT) <21000809@luxshare.com.cn>

--- AmiCRBPkg/Chipset/SB/SBPEI.c
+++ AmiCRBPkg/Chipset/SB/SBPEI.c
@@ -255,6 +255,8 @@ SBPEI_Init(
     PEI_TRACE((-1, PeiServices, "USE3_SEL = 0x%x\n", USE3_SEL ));
     PEI_TRACE((-1, PeiServices, "IO3_SEL = 0x%x\n", IO3_SEL ));
     PEI_TRACE((-1, PeiServices, "LVL3_SEL = 0x%x\n", LVL3_SEL ));
+    // Disable GPIO18 Blink
+    IoWrite32((UINT16)GPIO_BASE_ADDRESS + ICH_GPIO_GPO_BLINK, 0);
 #endif
 
     // Install the SB Init Policy PPI
